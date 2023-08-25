2f00536e12128283db5d95364a2fe69a8007a33d
Shah Danish <shah.official1011@gmail.com>
2023-08-25 14:15:55 +0530

    A simple Movie API fetcher website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import { Container, Heading, Center } from '@chakra-ui/react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container alignItems="center">
+      <Heading fontSize="45px" textAlign="center" color="orange" mt="50px">
+        Movie Finder
+      </Heading>
+      <Center>
+        <form action="/search" method="GET"> 
+          <div class="search-container">
+            <input type="text" class="custom-input" name="q" placeholder="Search..."/>
+              <button class="search-button" type="submit">
+                <svg class="search-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M15.5 14h-.8l-.3-.3a6.5 6.5 0 1 0-.7.7l.3.3v.8l4 3.9 2.3-2.3-3.9-4zm-6 0a4.5 4.5 0 1 1 0-9 4.5 4.5 0 0 1 0 9z"/>
+                </svg>
+              </button>
+          </div>
+        </form>       
+      </Center>
+    </Container>
   );
 }
 

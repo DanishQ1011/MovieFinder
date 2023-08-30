c7bb120c40cc17db337617366e80068253d01b25
Shah Danish <shah.official1011@gmail.com>
2023-08-30 17:39:53 +0530

    Background added using a package -> "tsparticles"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import { Container, Heading, Center } from '@chakra-ui/react';
 import './App.css';
+import ParticleBg from './components/ParticleBg';
+import MovieCard from './components/MovieCard';
+import { Container, Heading, Center } from '@chakra-ui/react';
 
 function App() {
+
   return (
+    <>
+    <ParticleBg />
+    
     <Container alignItems="center">
-      <Heading fontSize="45px" textAlign="center" color="orange" mt="50px">
-        Movie Finder
-      </Heading>
-      <Center>
-        <form action="/search" method="GET"> 
-          <div class="search-container">
-            <input type="text" class="custom-input" name="q" placeholder="Search..."/>
+        <Heading fontSize="45px" textAlign="center" color="orange" mt="50px">
+          Movie Finder
+        </Heading>
+        <Center>
+          <form action="/search" method="GET">
+            <div class="search-container">
+              <input type="text" class="custom-input" name="q" placeholder="Search..." />
               <button class="search-button" type="submit">
                 <svg class="search-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M15.5 14h-.8l-.3-.3a6.5 6.5 0 1 0-.7.7l.3.3v.8l4 3.9 2.3-2.3-3.9-4zm-6 0a4.5 4.5 0 1 1 0-9 4.5 4.5 0 0 1 0 9z"/>
+                  <path d="M15.5 14h-.8l-.3-.3a6.5 6.5 0 1 0-.7.7l.3.3v.8l4 3.9 2.3-2.3-3.9-4zm-6 0a4.5 4.5 0 1 1 0-9 4.5 4.5 0 0 1 0 9z" />
                 </svg>
               </button>
-          </div>
-        </form>       
-      </Center>
+            </div>
+          </form>
+        </Center>
     </Container>
+
+    <MovieCard/>
+    </>
+  
   );
 }
 
--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,7 +1,22 @@
+import { Card, CardBody, CardFooter, CardHeader, SimpleGrid, Text } from '@chakra-ui/react'
 import React from 'react'
 
 export default function MovieCard() {
   return (
-    <div>MovieCard</div>
+    <SimpleGrid spacing={10} minChildWidth="300px">
+        <Card color="white">
+           <CardHeader>
+            <Text>Card Header</Text>
+           </CardHeader>
+
+           <CardBody>
+            <Text>Card body</Text>
+           </CardBody>
+
+           <CardFooter>
+           <Text>Card footer</Text>
+           </CardFooter>   
+        </Card>
+    </SimpleGrid>
   )
 }

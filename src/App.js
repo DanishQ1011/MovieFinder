b65fd143d718cf4d5efd4e89f254895d8a3134d3
Shah Danish <shah.official1011@gmail.com>
2023-09-01 14:06:57 +0530

    API issue fixed

--- src/App.js
+++ src/App.js
@@ -4,79 +4,94 @@ import MovieCard from './components/MovieCard';
 import { Container, Heading, Center } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 
+function App() {
+  const API_KEY = 'db9b2282'; // Your OMDb API key
+  const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY}`;
 
-//db9b2282 - OMDb API 
+  const [searchTerm, setSearchTerm] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
 
-  
-
-  const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=db9b2282';
-
-  const App = () => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [movies, setMovies] = useState([]);
-
-    useEffect(() => {
-      //searchMovies();
-    }, [searchTerm]);
-
-    const searchMovies = async (title) => {
+  const searchMovies = async (title) => {
+    try {
       const response = await fetch(`${API_URL}&s=${title}`);
       const data = await response.json();
-    
-      setMovies(data.Search);
-    }
 
-    
+      if (data.Response === 'True') {
+        setMovies(data.Search);
+        setError(null);
+      } else {
+        setMovies([]);
+        setError(data.Error);
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setError('An error occurred while fetching data.');
+    }
+  };
+
+  useEffect(() => {
+    // Initially, you might want to load some default movies.
+    // searchMovies('Batman'); // Uncomment this line to load default movies.
+  }, []);
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      searchMovies(searchTerm);
+    }
+  };
+
   return (
     <>
-    <ParticleBg />
-    
-    <Container alignItems="center">
+      <ParticleBg />
+
+      <Container alignItems="center">
         <Heading fontSize="45px" textAlign="center" color="orange" mt="50px">
           Movie Finder
         </Heading>
         <Center>
-          <form action="/search" method="GET">
-            <div class="search-container">
-              <input 
-                type="text" 
-                class="custom-input" 
-                name="q" 
-                placeholder="Search..." 
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}/>
-              <button 
-                class="search-button" 
-                type="submit"
-                onClick={() => searchMovies(searchTerm)}>
-                <svg class="search-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M15.5 14h-.8l-.3-.3a6.5 6.5 0 1 0-.7.7l.3.3v.8l4 3.9 2.3-2.3-3.9-4zm-6 0a4.5 4.5 0 1 1 0-9 4.5 4.5 0 0 1 0 9z" />
-                </svg>
-              </button>
-            </div>
-          </form>
+          <div className="search-container">
+            <input
+              type="text"
+              className="custom-input"
+              name="q"
+              placeholder="Search..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              onKeyDown={handleKeyPress}
+            />
+            <button
+              className="search-button"
+              type="button"
+              onClick={() => searchMovies(searchTerm)}
+            >
+              <svg
+                className="search-icon"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M15.5 14h-.8l-.3-.3a6.5 6.5 0 1 0-.7.7l.3.3v.8l4 3.9 2.3-2.3-3.9-4zm-6 0a4.5 4.5 0 1 1 0-9 4.5 4.5 0 0 1 0 9z" />
+              </svg>
+            </button>
+          </div>
         </Center>
-    </Container>
+      </Container>
 
-    {
-      movies?.length > 0
-      ?(
-        <Container>
-          {movies.map((movie) => (
-            <MovieCard movie={movie}/>
-          ))}
-        </Container>
-      ) : (
+      {error ? (
         <div className="empty">
-          <h2>No Movies found</h2>
+          <h2>{error}</h2>
         </div>
-      )
-
-    }
-
-    
+      ) : (
+        movies.length > 0 && (
+          <Container>
+            {movies.map((movie, index) => (
+              <MovieCard key={index} movie={movie} />
+            ))}
+          </Container>
+        )
+      )}
     </>
-  
   );
 }
+
 export default App;
